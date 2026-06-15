e19849647fadc325a319ce6cba01759c8e7d3807
Sanskar Chandrakant Gambhir <156572240+SanskarGambhir@users.noreply.github.com>
2026-06-15 19:07:38 +0530

    Add: max-water-container - submission-3

---
+++ Data Structures & Algorithms/max-water-container/submission-3.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int maxArea(vector<int>& heights) {
+        int n = heights.size();
+        int left = 0;
+        int right = n-1;
+        int maxArea = 0;
+
+        while(left<right){
+            int area = min(heights[left], heights[right]) * (right-left);
+            maxArea = max(maxArea, area);
+
+            if(heights[left] < heights[right]){
+                left++;
+            }
+            else{
+                right--;
+            }
+        }
+
+        return maxArea;
+    }
+};
