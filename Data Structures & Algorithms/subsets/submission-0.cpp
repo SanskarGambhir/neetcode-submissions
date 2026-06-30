3e42e1c0aeea9134a7a11db4e7a278b76a10c7c5
Sanskar Chandrakant Gambhir <156572240+SanskarGambhir@users.noreply.github.com>
2026-06-30 13:11:27 +0530

    Add: subsets - submission-0

---
+++ Data Structures & Algorithms/subsets/submission-0.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<int> storage;
+        vector<vector<int>> result;
+        subsetsFunction(0, nums, storage, result);
+        return result;
+    }
+
+    void subsetsFunction(int index, vector<int>& nums, vector<int>& storage, vector<vector<int>>& result){
+        if(index==nums.size()){
+            result.push_back(storage);
+            return;
+        }
+
+        storage.push_back(nums[index]);
+        subsetsFunction(index+1, nums, storage, result);
+        storage.pop_back();
+
+        subsetsFunction(index+1, nums, storage, result);
+    }
+};
