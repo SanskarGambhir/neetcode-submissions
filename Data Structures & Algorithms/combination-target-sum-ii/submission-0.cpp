a92b9a72413dca52595e124a305389698ab7311c
Sanskar Chandrakant Gambhir <156572240+SanskarGambhir@users.noreply.github.com>
2026-06-30 12:44:32 +0530

    Add: combination-target-sum-ii - submission-0

---
+++ Data Structures & Algorithms/combination-target-sum-ii/submission-0.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+        vector<int> storage;
+        vector<vector<int>> result;
+        sort(candidates.begin(), candidates.end());
+        combinationSum2Function(0, target, candidates, storage, result);
+        return result;
+    }
+
+    void combinationSum2Function(int index, int target, vector<int>& candidates, vector<int>& storage, vector<vector<int>>& result){
+        if(target==0){
+            result.push_back(storage);
+            return;
+        }
+
+        if(index==candidates.size()){
+            return;
+        }
+
+        if(candidates[index]<=target){
+            storage.push_back(candidates[index]);
+            combinationSum2Function(index+1, target-candidates[index], candidates, storage, result);
+            storage.pop_back();
+        }
+
+        while(index+1<candidates.size() && candidates[index]==candidates[index+1]){
+            index++;
+        }
+
+        combinationSum2Function(index+1, target, candidates, storage, result);
+    }
+};
